cdee678dd9a71ac9227d13b3d84b4b1746cceb0e
chenxudong2020 <61214170+chenxudong2020@users.noreply.github.com>
2020-05-13 06:52:55 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -6,7 +6,18 @@
 # Blog: https://p3terx.com
 #=================================================
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.9.1/g' package/base-files/files/bin/config_generate
+
+
+
+
+device_name='PSG1218'
+wifi_name='PHICOMM'
+lan_ip='192.168.2.1'        # Lan Ip地址
+utc_name='Asia\/Shanghai'   # 时区
+
+wifischedule_url='https://github.com/newkit/luci-app-wifischedule.git' # wifi计划插件地址
+
+
 
 # Modify the version number
 sed -i 's/OpenWrt/Leopard build $(date "+%Y.%m.%d") @ OpenWrt/g' package/default-settings/files/zzz-default-settings
@@ -25,3 +36,27 @@ sed -i 's/OpenWrt/Leopard build $(date "+%Y.%m.%d") @ OpenWrt/g' package/default
 # rm rm -rf package/lean/v2ray
 # git clone https://github.com/KFERMercer/openwrt-v2ray.git package/lean/v2ray
 # git clone https://github.com/frainzy1477/luci-app-clash.git package/lean/luci-app-clash
+
+
+
+
+#修改机器名称
+echo "设置主机名"
+sed -i "s/OpenWrt/$device_name/g" package/base-files/files/bin/config_generate
+
+
+sed -i 's/192.168.1.1/$lan_ip/g' package/base-files/files/bin/config_generate
+
+#修改默认时区
+echo "修改时区"
+sed -i "s/'UTC'/'CST-8'\n   set system.@system[-1].zonename='$utc_name'/g" package/base-files/files/bin/config_generate
+
+
+echo "修改wifi名称"
+
+sed -i "s/OpenWrt/$wifi_name/g" package/kernel/mac80211/files/lib/wifi/mac80211.sh
+
+
+echo '添加wifi计划插件'
+git clone $wifischedule_url package/lean/luci-app-wifischedule
+echo 'CONFIG_PACKAGE_luci-app-wifischedule=y' >> .config
