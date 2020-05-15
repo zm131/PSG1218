de76e30b77a52ca168ecba450144838edbb50243
chenxudong2020 <61214170+chenxudong2020@users.noreply.github.com>
2020-05-16 07:20:43 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -17,26 +17,8 @@ utc_name='Asia\/Shanghai'   # 时区
 
 wifischedule_url='https://github.com/newkit/luci-app-wifischedule.git' # wifi计划插件地址
 
-
-
-# Modify the version number
-sed -i 's/OpenWrt/Leopard build $(date "+%Y.%m.%d") @ OpenWrt/g' package/default-settings/files/zzz-default-settings
-
-# 添加新的主题包
-# git clone https://github.com/Leo-Jo-My/luci-theme-opentomcat.git package/lean/luci-theme-opentomcat
-
-# 去除默认主题
-# sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
-
-# 添加clash支持
-# git clone https://github.com/frainzy1477/luci-app-clash.git package/lean/luci-app-clash
-
-# 添加v2ray
-# git clone https://github.com/kuoruan/luci-app-v2ray.git package/lean/luci-app-v2ray
-# rm rm -rf package/lean/v2ray
-# git clone https://github.com/KFERMercer/openwrt-v2ray.git package/lean/v2ray
-# git clone https://github.com/frainzy1477/luci-app-clash.git package/lean/luci-app-clash
-
+ 
+ 
 
 
 
@@ -58,5 +40,5 @@ sed -i "s/OpenWrt/$wifi_name/g" package/kernel/mac80211/files/lib/wifi/mac80211.
 
 
 echo '添加wifi计划插件'
-git clone $wifischedule_url package/lean/luci-app-wifischedule
+git clone $wifischedule_url package/luci-app-wifischedule
 echo 'CONFIG_PACKAGE_luci-app-wifischedule=y' >> .config
