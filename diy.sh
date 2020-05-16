cbb88299d29fd80f34dd5d45e3e6ed7cf7466590
chenxudong2020 <61214170+chenxudong2020@users.noreply.github.com>
2020-05-16 14:03:20 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -37,10 +37,3 @@ sed -i "s/'UTC'/'CST-8'\n   set system.@system[-1].zonename='$utc_name'/g" packa
 echo "修改wifi名称"
 
 sed -i "s/OpenWrt/$wifi_name/g" package/kernel/mac80211/files/lib/wifi/mac80211.sh
-
-
-echo '添加wifi计划插件'
-git clone $wifischedule_url package/luci-app-wifischedule
-echo 'CONFIG_PACKAGE_luci-app-wifischedule=y' >> .config
-
- 
