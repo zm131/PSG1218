b5401ca40fc4c2817013a2353057c2c9a56c78b7
chenxudong2020 <872603935@qq.com>
2020-07-14 13:02:35 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -8,9 +8,9 @@
 # Blog: https://p3terx.com
 #============================================================
 
-device_name='HC5962'
-wifi_name='HiWiFi'
-lan_ip='192.168.199.1'        # Lan Ip地址
+device_name='PSG1218'
+wifi_name='PHICOMM'
+lan_ip='192.168.2.1'        # Lan Ip地址
 utc_name='Asia\/Shanghai'   # 时区
 
 
@@ -28,7 +28,7 @@ sed -i "s/192.168.1.1/$lan_ip/g" package/base-files/files/bin/config_generate
 #修改默认时区
 echo "修改时区"
 sed -i "s/'UTC'/'CST-8'\n   set system.@system[-1].zonename='$utc_name'/g" package/base-files/files/bin/config_generate
-
+     
 
 echo "修改wifi名称"
 
