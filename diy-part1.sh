b2d229ad0b7ebe5524679c74f3546bcc28aa7a6a
zm131 <53132876+zm131@users.noreply.github.com>
2020-08-16 21:51:44 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,6 +17,6 @@
 #sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 
 # Define My Package
-#git clone https://github.com/fw876/helloworld.git                                   package/molun/luci-app-ssr-plus
-#git clone https://github.com/rufengsuixing/luci-app-adguardhome.git                 package/molun/luci-app-adguardhome
-#git clone https://github.com/vernesong/OpenClash.git                                package/molun/luci-app-openclash
+git clone https://github.com/fw876/helloworld.git                                   package/molun/luci-app-ssr-plus
+git clone https://github.com/rufengsuixing/luci-app-adguardhome.git                 package/molun/luci-app-adguardhome
+git clone https://github.com/vernesong/OpenClash.git                                package/molun/luci-app-openclash
